eb8a43b27d8acbea3faa6c79b67b8c4baa1e02f4
Lalrak <150138865+Lalrak@users.noreply.github.com>
2026-01-05 21:39:36 -0300

    Adicionar styled-components e refatorar componentes do cabeçalho; remover estilos não utilizados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import "./App.css";
 import Header from "./components/header/index.js";
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
+
+  li {
+    list-style: none;
+  }
+`;
 
 function App() {
   return (
-    <div className="app">
+    <AppContainer>
       <Header />
-    </div>
+    </AppContainer>
   );
 }
 
---
+++ src/components/opcoesHeader/opcoesHeader.component.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const textoOpcoes = ["Categorias", "Favoritos", "Minha Estante"];
+
+const Opcoes = styled.ul`
+  display: flex;
+`;
+
+const Opcao = styled.li`
+  min-width: 120px;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+`;
+
+function OpcoesHeader() {
+  return (
+    <Opcoes>
+      {textoOpcoes.map((texto) => (
+        <Opcao>
+          <p>{texto}</p>
+        </Opcao>
+      ))}
+    </Opcoes>
+  );
+}
+
+export default OpcoesHeader;
