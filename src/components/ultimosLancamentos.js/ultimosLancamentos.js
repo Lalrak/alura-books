5bdc1d6248950c4055efe1a5ee02db2aaeae2722
Lalrak <150138865+Lalrak@users.noreply.github.com>
2026-01-12 20:59:44 -0300

    Adicionar componente Titulo e integrar ao componente UltimosLancamentos; criar componente CardRecomenda para exibição de recomendações de livros

--- src/components/ultimosLancamentos.js/ultimosLancamentos.js
+++ src/components/ultimosLancamentos.js/ultimosLancamentos.js
@@ -1,22 +1,14 @@
 import styled from "styled-components";
-import { useState } from "react";
 import { livros } from "./dadosUltimosLancamentos.js";
+import { Titulo } from "../Titulo/Titulo.js";
+import CardRecomenda from "../cardRecomenda/cardRecomenda.js";
+import imagemLivro from "../../images/livro2.png";
 
 const UltimosLancamentosContainer = styled.section`
   background-color: #ebecee;
   padding-bottom: 20px;
   display: flex;
   flex-direction: column;
-`;
-
-const Titulo = styled.h2`
-  width: 100%;
-  padding: 30px 0;
-  background-color: #fff;
-  color: #eb9b00;
-  font-size: 36px;
-  text-align: center;
-  margin: 0;
 `;
 
 const NovosLivrosContainer = styled.div`
@@ -30,12 +22,20 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
   return (
     <UltimosLancamentosContainer>
-      <Titulo>ULTIMOS LANÇAMENTOS</Titulo>
+      <Titulo cor="#EB9B00" tamanhoFonte="36px" alinhamento="center">
+        ÚLTIMOS LANÇAMENTOS
+      </Titulo>
       <NovosLivrosContainer>
         {livros.map((l) => (
           <img src={l.src} alt="" />
         ))}
       </NovosLivrosContainer>
+      <CardRecomenda
+        titulo="Talvez você se interesse por"
+        subtitulo="Angular 11"
+        descricao="Construindo uma aplicação com a plataforma Google"
+        imagem={imagemLivro}
+      />
     </UltimosLancamentosContainer>
   );
 }
